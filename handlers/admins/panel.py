8ef375f1be340469b9fe06c7c8879427558a4ae8
coder <cybersnic@gmail.com>
2024-07-04 23:15:55 +0500

    The speed of the bot has been greatly increased and optimized, and additional features have been added

--- handlers/admins/panel.py
+++ handlers/admins/panel.py
@@ -55,18 +55,10 @@ async def chat_ids_handler(call: CallbackQuery):
             json.dump(users_data, users_file, indent=3)
         with open(groups_file_path, 'w') as groups_file:
             json.dump(groups_data, groups_file, indent=3)
-        user_msg = f"""<b>
-        
-        🤖 Bot:  @{BOT_USERNAME}
-        📄 Users count:  {len(users_data)}
-        
-        </b>"""
-        group_msg = f"""<b>
-        
-        🤖 Bot:  @{BOT_USERNAME}
-        📄 Groups count:  {len(groups_data)}
-        
-        </b>"""
+        user_msg = f"""<b>🤖 Bot:  @{BOT_USERNAME}
+        📄 Users count:  {len(users_data)}</b>"""
+        group_msg = f"""<b>🤖 Bot:  @{BOT_USERNAME}
+        📄 Groups count:  {len(groups_data)}</b>"""
         await bot.send_document(chat_id=call.message.chat.id, document=InputFile(users_file_path), caption=user_msg)
         await bot.send_document(chat_id=call.message.chat.id, document=InputFile(groups_file_path), caption=group_msg)
         os.remove(users_file_path)
